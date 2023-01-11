ddfd3c37a11b94ba4b98560485a52ed0466e2abe
Kevin Shaffer-Morrison <shafkevi@amazon.com>
2023-01-11 11:46:20 -0500

    init

---
+++ test/aws-ecs-processing-pipeline.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsEcsProcessingPipeline from '../lib/aws-ecs-processing-pipeline-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-ecs-processing-pipeline-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsEcsProcessingPipeline.AwsEcsProcessingPipelineStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
